653ee697434460a5d64af703b9c49eca6e8e5668
KeepersOfTheMageia <tobybeesley@hotmail.com>
2026-08-28 21:59:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const overlay = document.getElementById("image-overlay");
 const LargeImage = document.getElementById("large-image");
 const cardImages = document.querySelectorAll(".card img");
+
+cardImages.forEach(function(image) {
+  image.addEventListener("click", function() {
+    largeImage.src = image.src;
+    overlay.style.display = "flex";
+  });
+});
