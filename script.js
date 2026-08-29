0c97e88090082cf0b50a20c68aff072f14bec8d0
KeepersOfTheMageia <tobybeesley@hotmail.com>
2026-08-29 14:44:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const overlay = document.getElementById("image-overlay");
-const LargeImage = document.getElementById("large-image");
+const largeImage = document.getElementById("large-image");
 const cardImages = document.querySelectorAll(".card img");
 const closeButton = document.getElementById("close-button");
 
 cardImages.forEach(function(image) {
   image.addEventListener("click", function() {
-    LargeImage.src = image.src;
+    largeImage.src = image.src;
     overlay.style.display = "flex";
   });
 });
-LargeImage.addEventListener("click", function(event) {
+largeImage.addEventListener("click", function(event) {
   event.stopPropagation();
 });
               
